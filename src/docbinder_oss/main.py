6105f066377a8e6b5ea3694d8ea9142eee99ddf6
PaoloLeonard <paololeonard@skynet.be>
2025-06-12 22:33:42 +0200

    removed metadata

--- src/docbinder_oss/main.py
+++ src/docbinder_oss/main.py
@@ -4,7 +4,7 @@ from docbinder_oss.services.google_drive.google_drive_client import GoogleDriveC
 def hello_oss():
     google_drive_client = GoogleDriveClient()
     drives = google_drive_client.get_file_metadata(
-        "19QYV33CQLL0bHjr12-Fud4EMv-NEsqnxh52-bi72CM8"
+        "metadata"
     )
     logger.info(f"{drives}")
     return "Hello from docbinder-oss!"
